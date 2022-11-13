fcf6b88c909d3c97dee64d25612a29b05016cbc8
spykees <cedcrit@gmail.com>
2022-11-13 21:01:10 +0100

    loop while for

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
 
 i = 1
 
-#Loop until i = 10
+#Loop (while) until i = 10
 print("Starting Loop !")
 
 while i <= 10 :
@@ -10,4 +10,26 @@ while i <= 10 :
 
 print("End of loop !")
 
+# Loop (for) pour chaque
 
+for letter in "Hello World":
+    print(letter)
+
+friends = ["Alain", "Marc", "Noémie", "Jean", "Bertrand"]
+
+for name in friends:
+    print(name)
+
+for index in range(0, 11): #range n'inclus pas le dernier chiffre (ex: 0,10 affiche 1 à 9)
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+
+# Logic inside loop
+for index in range(5):
+    if index == 0:
+        print("First index")
+    else:
+        print("Not first")
