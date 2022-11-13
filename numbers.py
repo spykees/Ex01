1004dcb42878bb54899a849dc3ce6bffec9b5e55
spykees <cedcrit@gmail.com>
2022-11-13 03:02:43 +0100

    number and math

---
+++ numbers.py
@@ -0,0 +1,34 @@
+#importation du module Math
+from math import *
+
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(3 * 1 + 1) # (3x1) + 1
+print(3 * (1 + 1)) # 3x(1 + 1)
+print(10 % 3) # Modulo le reste
+
+my_num = 5
+print(my_num) #number
+
+# Nombre en string
+print(str(my_num)) # String
+print(str(my_num) + " Mon chiffre préféré")
+
+# Functions
+
+my_num = -5
+
+print(abs(my_num)) #absolute value
+print(pow(3, 2)) # Power of
+print(max(3, 2)) # Max value 
+print(min(3, 2)) # Min value
+print(round(3.2)) # Rounded (arrondit)
+print(round(3.6)) # Rounded (arrondit)
+
+# All math module functions can be found on :
+#  
+# https://docs.python.org/3/library/math.html
+# https://www.w3schools.com/python/module_math.asp
